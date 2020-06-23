9a7a0b7f8e016ca45b16120b78453feba98d0062
Ashley Caselli <ashley.caselli@studio.unibo.it>
2020-06-23 12:02:36 +0200

    Fix bug in gulp js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const gulp = require( 'gulp' );
-const sass = require( 'gulp-sass' );
-const postcss = require( 'gulp-postcss' );
-const autoprefixer = require( 'autoprefixer' );
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 const rename = require('gulp-rename');
 const dotenv = require('dotenv').config();
 const browserSync = require('browser-sync').create();
@@ -12,33 +12,33 @@ const destCssDir = "css";
 const destJSDir = "js";
 
 if (process.env.TEMPLATE_DIR) {
-    destDir = process.env.TEMPLATE_DIR ;
+    destDir = process.env.TEMPLATE_DIR;
 }
 
 function css(done) {
     var processors = [
-		autoprefixer( { browsers: ['last 2 versions'] } )
+        autoprefixer({browsers: ['last 2 versions']})
     ];
 
     var sheets = [
-        { src : './sass/style.scss', dest : `${destDir}/${destCssDir}/` }
+        {src: './sass/style.scss', dest: `${destDir}/${destCssDir}/`}
     ];
-    
+
     sheets.forEach(file => {
-        return gulp.src( file.src )
-            .pipe( sass( {outputStyle: 'compressed'} ).on( 'error', sass.logError ) )
-            .pipe( rename( { suffix: '.min' } ) )
-            .pipe( postcss( processors ) )
-            .pipe( gulp.dest( file.dest ));
+        return gulp.src(file.src)
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(postcss(processors))
+            .pipe(gulp.dest(file.dest));
     });
     done();
 }
 
 function js() {
-    return gulp.src( 'js/*.js')
-        .pipe( uglify() )
-        .pipe( rename({ suffix: '.min' }) )
-        .pipe( gulp.dest( `${destDir}/${destJSDir}`) );
+    return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(`${destDir}/${destJSDir}`));
 }
 
 function bs(done) {
@@ -50,9 +50,9 @@ function watch() {
     browserSync.init({
         proxy: `${process.env.HOST}:${process.env.PORT}`
     });
-    gulp.watch( 'sass/**/*.scss', gulp.series( css, js, bs ));   
+    gulp.watch('sass/**/*.scss', gulp.series(css, js, bs));
 }
 
 exports.css = css;
 exports.js = js;
-exports.default = gulp.series( css, js, watch );
+exports.default = gulp.series(css, js, watch);
